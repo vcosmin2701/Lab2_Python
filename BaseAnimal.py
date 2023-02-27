3f532dfc77c0f902042df78dfc3a60cd413b8d27
vcosmin2701 <cosmin.vladut@ulbsibiu.ro>
2023-02-27 13:27:01 +0200

    Inheritance Python

---
+++ BaseAnimal.py
@@ -0,0 +1,8 @@
+class BaseAnimal:
+    def manaca_mancare(self):
+        print("Animalul mananca mancare")
+
+
+class PisicaMeow:
+    def mananca_mancare(self):
+        print("Pisica mananca mancare ")
--- Main.py
+++ Main.py
@@ -1,1 +1,8 @@
-print("Hello World")
+import BaseAnimal
+
+first_animal = BaseAnimal.BaseAnimal()
+first_animal.manaca_mancare()
+
+first_pisica = BaseAnimal.PisicaMeow()
+first_pisica.mananca_mancare()
+
